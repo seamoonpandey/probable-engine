7822573e45d48790cbd069249f3b580e46c21c27
Seamoon Pandey <hackedasocial@gmail.com>
2023-12-09 11:25:53 +0545

    add screen layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,11 @@ export default function RootLayout({
     <html lang="en" className='h-full'>
       <body
         className={cn("relative  h-full font-sans antialiased", inter.className)}>
-        {children}
+        <main className="relative flex flex-col min-h-screen">
+          <div className='flex-grow flex-1'>
+            {children}
+          </div>
+        </main>
       </body>
     </html>
   )
