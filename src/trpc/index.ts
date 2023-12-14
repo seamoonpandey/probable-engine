54b8704605151ca4201c7a4c4d0ddff72ed5f238
Seamoon Pandey <hackedasocial@gmail.com>
2023-12-14 19:16:53 +0545

    lint

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,7 +1,7 @@
 import { publicProcedure, router } from "./trpc";
 
 export const appRouter = router({
-    anyApiRoute: publicProcedure.query(()=>{
+    anyApiRoute: publicProcedure.query(() => {
         return 'hello'
     }),
 })
