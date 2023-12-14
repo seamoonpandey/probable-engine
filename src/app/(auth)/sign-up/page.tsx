38482bc89ff514bb15e3ffc2c47702898e17aae0
Seamoon Pandey <hackedasocial@gmail.com>
2023-12-14 17:42:13 +0545

    add signup page

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { Icons } from "@/components/Icons";
+import { Button, buttonVariants } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { cn } from "@/lib/utils";
+import { Label } from "@radix-ui/react-label";
+import Link from "next/link";
+
+const Page = () => {
+  return (
+    <>
+      <div className="container relative flex pt-20 flex-col items-center justify-center lg:px-0">
+        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+          <div className="flex flex-col items-center space-y-2 text-center">
+            <Icons.logo className="h-20 w-20" />
+            <h1 className="text-2xl font-bold">Create an account</h1>
+
+            <Link
+              href="/sign-in"
+              className={buttonVariants({
+                variant: "link",
+                className: "gap-1.5",
+              })}
+            >
+              Already have an account? Sign-in
+            </Link>
+          </div>
+
+          <div className="grid gap-6">
+            <form onSubmit={() => {}}>
+              <div className="grid gap-2">
+                <div className="grid gap-1 py-2">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    className={cn({
+                      "focus-visible:ring-red-500": true,
+                    })}
+                    placeholder="you@example.com"
+                  />
+                </div>
+                <div className="grid gap-1 py-2">
+                  <Label htmlFor="email">Password</Label>
+                  <Input
+                    className={cn({
+                      "focus-visible:ring-red-500": true,
+                    })}
+                    placeholder="your_password"
+                  />
+                </div>
+                <Button>Sign up</Button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Page;
