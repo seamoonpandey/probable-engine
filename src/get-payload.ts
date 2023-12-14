6d2fa92d5e01efc3e4e8f3b6ffbb242533fe7d1c
Seamoon Pandey <hackedasocial@gmail.com>
2023-12-14 20:09:44 +0545

    add resend api key

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -2,9 +2,20 @@ import dotenv from "dotenv";
 import path from "path";
 import payload, { Payload } from "payload";
 import type { InitOptions } from "payload/config";
+import nodemailer from 'nodemailer';
 
 dotenv.config({
     path: path.resolve(__dirname, "../.env")
+})
+
+const transporter = nodemailer.createTransport({
+    host: "smpt.resend.com",
+    secure: true,
+    port: 465,
+    auth: {
+        user: "resend",
+        pass: process.env.RESEND_API_KEY
+    }
 })
 
 let cached = (global as any).payload
