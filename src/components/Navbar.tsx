d68d5d333f944f0d06952795d30bcb922ede881f
Seamoon Pandey <hackedasocial@gmail.com>
2023-12-15 00:03:09 +0545

    add user detail dynamic nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,77 +1,80 @@
-import Link from 'next/link'
-import MaxWidthWrapper from './MaxWidthWrapper'
-import { Icons } from './Icons'
-import NavItems from './NavItems'
-import { buttonVariants } from './ui/button'
-import Cart from './Cart'
+"use server";
+import Link from "next/link";
+import MaxWidthWrapper from "./MaxWidthWrapper";
+import { Icons } from "./Icons";
+import NavItems from "./NavItems";
+import { buttonVariants } from "./ui/button";
+import Cart from "./Cart";
+import { getServerSideUser } from "@/lib/payload-utils";
+import { cookies } from "next/headers";
+import UserAccountNav from "./UserAccountNav";
 
-const Navbar =  () => {
-const user = null
+const Navbar = async () => {
+  const nextCookies = cookies();
+  const { user } = await getServerSideUser(nextCookies);
   return (
-    <div className='bg-white sticky z-50 top-0 inset-x-0 h-16'>
-      <header className='relative bg-white'>
+    <div className="bg-white sticky z-50 top-0 inset-x-0 h-16">
+      <header className="relative bg-white">
         <MaxWidthWrapper>
-          <div className='border-b border-gray-200'>
-            <div className='flex h-16 items-center'>
-{/* TODO: mobile nav */}
-              <div className='ml-4 flex lg:ml-0'>
-                <Link href='/'>
-                  <Icons.logo className='h-10 w-10' />
+          <div className="border-b border-gray-200">
+            <div className="flex h-16 items-center">
+              {/* TODO: mobile nav */}
+              <div className="ml-4 flex lg:ml-0">
+                <Link href="/">
+                  <Icons.logo className="h-10 w-10" />
                 </Link>
               </div>
 
-              <div className='hidden z-50 lg:ml-8 lg:block lg:self-stretch'>
+              <div className="hidden z-50 lg:ml-8 lg:block lg:self-stretch">
                 <NavItems />
               </div>
 
-              <div className='ml-auto flex items-center'>
-                <div className='hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6'>
+              <div className="ml-auto flex items-center">
+                <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
                   {user ? null : (
                     <Link
-                      href='/sign-in'
+                      href="/sign-in"
                       className={buttonVariants({
-                        variant: 'ghost',
-                      })}>
+                        variant: "ghost",
+                      })}
+                    >
                       Sign in
                     </Link>
                   )}
 
-                  {user ? null : (
-                    <span
-                      className='h-6 w-px bg-gray-200'
-                      aria-hidden='true'
-                    />
+                  {user ? (
+                    <UserAccountNav user={user} />
+                  ) : (
+                    <span className="h-6 w-px bg-gray-200" aria-hidden="true" />
                   )}
 
                   {user ? (
                     <p></p>
                   ) : (
                     <Link
-                      href='/sign-up'
+                      href="/sign-up"
                       className={buttonVariants({
-                        variant: 'ghost',
-                      })}>
+                        variant: "ghost",
+                      })}
+                    >
                       Create account
                     </Link>
                   )}
 
                   {user ? (
-                    <span
-                      className='h-6 w-px bg-gray-200'
-                      aria-hidden='true'
-                    />
+                    <span className="h-6 w-px bg-gray-200" aria-hidden="true" />
                   ) : null}
 
                   {user ? null : (
-                    <div className='flex lg:ml-6'>
+                    <div className="flex lg:ml-6">
                       <span
-                        className='h-6 w-px bg-gray-200'
-                        aria-hidden='true'
+                        className="h-6 w-px bg-gray-200"
+                        aria-hidden="true"
                       />
                     </div>
                   )}
 
-                  <div className='ml-4 flow-root lg:ml-6'>
+                  <div className="ml-4 flow-root lg:ml-6">
                     <Cart />
                   </div>
                 </div>
@@ -81,7 +84,7 @@ const user = null
         </MaxWidthWrapper>
       </header>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
