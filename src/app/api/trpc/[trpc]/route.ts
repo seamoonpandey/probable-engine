33d5f5ebc6faa72bb7d0ce10eef86461c99c530e
Seamoon Pandey <hackedasocial@gmail.com>
2023-12-14 23:22:44 +0545

    eslint ignore

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -7,6 +7,7 @@ const handler = (req: Request) => {
         endpoint: "/api/trpc",
         req,
         router: appRouter,
+        // @ts-expect-error contextalready passed from express middleware
         createContext: () => ({}),
     })
 }
