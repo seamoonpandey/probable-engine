b6367bfe5c3806d18840615d03f55dc8c5d3a70d
Seamoon Pandey <hackedasocial@gmail.com>
2023-12-15 07:55:18 +0545

    add product and media model

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -3,8 +3,10 @@ import { slateEditor } from "@payloadcms/richtext-slate";
 import { buildConfig } from "payload/config";
 import { webpackBundler } from "@payloadcms/bundler-webpack";
 import path from "path";
-import { Users } from "./collections/Users";
 import dotenv from "dotenv";
+import { Users } from "./collections/Users";
+import { Products } from "./collections/Products";
+import { Media } from "./collections/Media";
 
 dotenv.config({
     path: path.resolve(__dirname, '../.env')
@@ -15,7 +17,7 @@ dotenv.config({
 export default buildConfig({
     serverURL: process.env.NEXT_PUBLIC_SERVER_URL || "",
     collections: [
-        Users
+        Users, Products, Media
     ],
     routes: {
         admin: '/sell'
